d98bebc35408bde76b05be4d861fff21e23029ee
Yakiv Yusin <yusin.yakiv@gmail.com>
2021-06-15 19:18:20 +0300

    Added Models, Helpers to Sample Project

---
+++ MTaaS.Sample/Helpers/StringWriterComparer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace MTaaS.Sample.Helpers
+{
+    public class StringWriterComparer : IEqualityComparer<StringWriter>
+    {
+        public bool Equals([AllowNull] StringWriter x, [AllowNull] StringWriter y)
+        {
+            if (x == null && y == null)
+            {
+                return true;
+            }
+
+            return x?.ToString().Equals(y?.ToString()) ?? false;
+        }
+
+        public int GetHashCode([DisallowNull] StringWriter obj)
+        {
+            return obj.ToString().GetHashCode();
+        }
+    }
+}
