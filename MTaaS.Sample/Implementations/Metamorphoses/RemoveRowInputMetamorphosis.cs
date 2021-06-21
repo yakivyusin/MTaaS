6bb2b0b353d326ee742891ee43977217dcce90d2
Yakiv Yusin <yusin.yakiv@gmail.com>
2021-06-21 12:43:16 +0300

    Added Remove Row MR

---
+++ MTaaS.Sample/Implementations/Metamorphoses/RemoveRowInputMetamorphosis.cs
@@ -0,0 +1,15 @@
+﻿using MTaaS.Sample.Models;
+using System.Linq;
+
+namespace MTaaS.Metamorphoses
+{
+    public partial class RemoveRowInputMetamorphosis
+    {
+        public partial TableItem[] Transform(TableItem[] input)
+        {
+            return input
+                .Take(input.Length - 1)
+                .ToArray();
+        }
+    }
+}
---
+++ MTaaS.Sample/Implementations/Metamorphoses/RemoveRowOutputMetamorphosis.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace MTaaS.Metamorphoses
+{
+    public partial class RemoveRowOutputMetamorphosis
+    {
+        public partial StringWriter Transform(StringWriter output)
+        {
+            var outputString = output.ToString();
+            var stringBuilder = new StringBuilder(outputString);
+
+            var lastIndex = outputString.LastIndexOf(Environment.NewLine, outputString.Length - 2);
+            var prelastIndex = outputString.LastIndexOf(Environment.NewLine, lastIndex);
+
+            stringBuilder.Remove(prelastIndex, outputString.Length - prelastIndex);
+            stringBuilder.AppendLine();
+            
+            return new StringWriter(stringBuilder);
+        }
+    }
+}
