65817d469c1789d8dbe05b9fee239245bc64ecaf
Yakiv Yusin <yusin.yakiv@gmail.com>
2022-03-12 16:08:10 +0200

    Add Attributes Set

---
+++ MTaaS.Attributes/ArtifactEntryPointAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MTaaS.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+    public class ArtifactEntryPointAttribute : Attribute
+    {
+        public string RelationName { get; }
+
+        public ArtifactEntryPointAttribute(string relationName)
+        {
+            RelationName = relationName;
+        }
+    }
+}
---
+++ MTaaS.Attributes/Metamorphoses/OutputMetamorphosisAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MTaaS.Attributes.Metamorphoses
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+    public class OutputMetamorphosisAttribute : Attribute
+    {
+        public string RelationName { get; }
+
+        public OutputMetamorphosisAttribute(string relationName)
+        {
+            RelationName = relationName;
+        }
+    }
+}
