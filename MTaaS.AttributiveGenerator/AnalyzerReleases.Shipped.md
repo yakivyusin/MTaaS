05d4edaac11764e9eeed8bfe7d94410fed3ba549
Yakiv Yusin <yusin.yakiv@gmail.com>
2022-03-15 09:57:05 +0200

    Add Attributive Source Generator

---
+++ MTaaS.AttributiveGenerator/AnalyzerReleases.Shipped.md
@@ -0,0 +1,14 @@
+﻿; Shipped analyzer releases
+; https://github.com/dotnet/roslyn-analyzers/blob/master/src/Microsoft.CodeAnalysis.Analyzers/ReleaseTrackingAnalyzers.Help.md
+
+## Release 1.0
+
+### New Rules
+Rule ID | Category | Severity | Notes
+--------|----------|----------|-------
+MTAAS002 | MT-as-a-Service | Error | MTaaSGenerator
+MTAAS003 | MT-as-a-Service | Error | MTaaSGenerator
+MTAAS004 | MT-as-a-Service | Error | MTaaSGenerator
+MTAAS005 | MT-as-a-Service | Error | MTaaSGenerator
+MTAAS006 | MT-as-a-Service | Error | MTaaSGenerator
+MTAAS007 | MT-as-a-Service | Error | MTaaSGenerator
---
+++ MTaaS.AttributiveGenerator/ClassSyntaxReceiver.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MTaaS.AttributiveGenerator
+{
+    internal class ClassSyntaxReceiver : ISyntaxContextReceiver
+    {
+        private readonly Dictionary<string, List<(INamedTypeSymbol Type, string Relation)>> _types = new()
+        {
+            [Constants.InputMetamorphosisAttribute] = new(),
+            [Constants.OutputMetamorphosisAttribute] = new(),
+            [Constants.ArtifactEntryPointAttribute] = new(),
+            [Constants.DataGeneratorAttribute] = new(),
+            [Constants.OutputModelComparerAttribute] = new()
+        };
+
+        public IReadOnlyDictionary<string, List<(INamedTypeSymbol Type, string Relation)>> Types => _types;
+
+        public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
+        {
+            if (context.Node is not ClassDeclarationSyntax classDeclarationNode)
+            {
+                return;
+            }
+
+            var typeSymbol = context.SemanticModel.GetDeclaredSymbol(classDeclarationNode) as INamedTypeSymbol;
+
+            var patternAttributes = typeSymbol
+                .GetAttributes()
+                .Where(x => _types.Keys.Contains(x.AttributeClass.ToDisplayString()))
+                .ToArray();
+
+            foreach (var patternAttribute in patternAttributes)
+            {
+                _types[patternAttribute.AttributeClass.ToDisplayString()]
+                    .Add((typeSymbol, patternAttribute.ConstructorArguments.First().Value.ToString()));
+            }
+        }
+    }
+}
